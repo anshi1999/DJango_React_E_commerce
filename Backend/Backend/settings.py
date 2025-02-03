c884664ad83984d4dffdda3de5a41ed148ce8899
anshi1999 <141669278+anshi1999@users.noreply.github.com>
2025-02-03 22:23:42 +0530

    day2 commit

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'userauths',
 ]
+
 
 # customer Apps
 'store'
@@ -129,7 +131,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = BASE_DIR / 'media' 
-# Default primary key field type
+
+AUTH_USER_MODEL = 'userauths.User'
+
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -1,26 +1,16 @@
-"""
-URL configuration for Backend project.
+from django.contrib import admin  # type: ignore
+from django.urls import path  # type: ignore
+from django.conf import settings  # type: ignore
+from django.conf.urls.static import static  # type: ignore
+from userauths.views import home
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin # type: ignore
-from django.urls import path # type: ignore
-from django.conf import settings # type: ignore
-from django.conf.urls.static import static # type: ignore
+# Define a simple view for the root URL
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
+# Serve media and static files in development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
